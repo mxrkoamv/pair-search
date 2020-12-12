7166a5f9664293bd8dd80b2e845dae605afd039a
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-12-12 16:09:27 -0500

    New gitignore and module-info

--- src/controlador/Menu.java
+++ src/controlador/Menu.java
@@ -6,7 +6,6 @@
 package controlador;
 
 import javafx.application.Application;
-;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module pair.search {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens controlador;
+}
